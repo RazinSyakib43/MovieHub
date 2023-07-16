5c46517a5e9a4d41b13589ecdad560536673ca2d
Razin Syakib <muhammadrazin14@gmail.com>
2023-07-17 06:55:12 +0700

    Add movie search functionality using OMDB API
    Implement movie search functionality in the 'App' component by fetching data from the OMDB API. The 'searchMovies' function makes an API request using fetch and displays the retrieved movie data in the console. Used the useEffect hook to call 'searchMovies' on component mount, searching for movies with the title 'Batman'.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import {useEffect} from "react";
+
+// c0fc28e7
+
+const API_URL = "http://www.omdbapi.com/?apikey=c0fc28e7";
 
 const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchMovies('Batman');
+  }, []);
+
   return (
     <div>
       <h1>App</h1>
