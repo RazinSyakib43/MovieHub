4245f65aed0f748d7a61adf1bb85e00eec67214b
Razin Syakib <muhammadrazin14@gmail.com>
2023-07-17 08:32:24 +0700

    Implement movie search and dynamic movie rendering
    Integrate movie search functionality using useState to store the movie data fetched from the OMDB API. Render the movie cards dynamically based on the fetched movies using the MovieCard component. Display 'No movies found' message if there are no movies returned.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
-import {useEffect} from "react";
+import {useState, useEffect} from "react";
+
+import MovieCard from "./MovieCard";
 
 import "./App.css";
 import Searchicon from "./search.svg";
@@ -15,10 +17,12 @@ const movie1 = {
 };
 
 const App = () => {
+  const [movies, setMovies] = useState([]);
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
-    console.log(data);
+    setMovies(data.Search);
   };
 
   useEffect(() => {
@@ -38,9 +42,19 @@ const App = () => {
         <img src={Searchicon} alt="search" onClick={() => {}} />
       </div>
 
-      <div className="container">
-        <MovieCard movie1={movie1} />
-      </div>
+      {movies.length > 0 ? (
+        <div className="container">
+          {
+            movies.map((movie) => (
+              <MovieCard movie={movie} />
+            ))
+          }
+        </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 }
