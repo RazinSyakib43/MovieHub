370288424fe1251f7ad9cf044fbb184e2182bf31
Razin Syakib <muhammadrazin14@gmail.com>
2023-07-17 08:04:22 +0700

    Try to fetch and display movie data from JSON
    Try to fetching and displaying movie data from a JSON source. Use a JSON object movie1 to store movie details, including title, year, imdbID, type, and poster URL. Render the movie details within the 'container' div with CSS styling applied.

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,15 @@ import Searchicon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com/?apikey=c0fc28e7";
 
+const movie1 = {
+  Title: "Batman v Superman: Dawn of Justice",
+  Year: "2016",
+  imdbID: "tt2975590",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BYThjYzcyYzItNTVjNy00NDk0LTgwMWQtYjMwNmNlNWJhMzMyXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+};
+
 const App = () => {
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -27,6 +36,21 @@ const App = () => {
           onChange={() => {}}
         />
         <img src={Searchicon} alt="search" onClick={() => {}} />
+      </div>
+
+      <div className="container">
+        <div className="movie">
+          <div>
+            <p>{movie1.Year}</p>
+          </div>
+          <div>
+            <img src={movie1.Poster} alt={movie1.Title} />
+          </div>
+          <div>
+            <span>{movie1.Type}</span>
+            <h3>{movie1.Title}</h3>
+          </div>
+        </div>
       </div>
     </div>
   );
